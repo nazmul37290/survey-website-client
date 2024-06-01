cc663284a3685437f8f0922dda268671f560bc31
nazmul37290 <nazmulnjm@gmail.com>
2024-06-01 10:53:04 +0600

    added banner section

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -1,22 +1,26 @@
+import bannerBg from "../../assets/Images/banner-bg.jpg";
 const Banner = () => {
   return (
     <div
-      className="hero min-h-screen"
+      className="hero h-[650px]"
       style={{
-        backgroundImage:
-          "url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)",
+        backgroundImage: `url(${bannerBg})`,
       }}
     >
-      <div className="hero-overlay bg-opacity-60"></div>
-      <div className="hero-content text-center text-neutral-content">
-        <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+      <div className="hero-overlay bg-opacity-80"></div>
+      <div className="hero-content text-center text-white">
+        <div className="max-w-2xl">
+          <h1 className="mb-5 text-5xl font-bold">
+            Welcome to <span className="text-main">SurveyStream</span>
+          </h1>
           <p className="mb-5">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+            Effortlessly create, share, and analyze surveys with SurveyStream.
+            Harness valuable insights and make data-driven decisions with ease.
+            Dive into the stream of feedback today!
           </p>
-          <button className="btn btn-primary">Get Started</button>
+          <button className="btn bg-main border-none text-second font-bold px-10 text-base">
+            Explore
+          </button>
         </div>
       </div>
     </div>

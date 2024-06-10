9c9a884669a369feae8788676b8f2483face8471
nazmul37290 <nazmulnjm@gmail.com>
2024-06-10 22:46:45 +0600

    added feedback section for surveyor

--- src/Pages/Surveyor/Feedback.jsx
+++ src/Pages/Surveyor/Feedback.jsx
@@ -1,9 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 import SectionTitle from "../../components/shared/SectionTitle";
 import useAxiosSecure from "../../hooks/useAxiosSecure";
+import useSurveys from "../../hooks/useSurveys";
 
 const Feedback = () => {
   const axiosSecure = useAxiosSecure();
+  const [surveys] = useSurveys();
   const {
     data: feedbacks = [],
     isPending,
@@ -11,16 +13,51 @@ const Feedback = () => {
   } = useQuery({
     queryKey: ["feedbacks"],
     queryFn: async () => {
-      const res = await axiosSecure.get("/surveys/");
+      const res = await axiosSecure.get("/surveys/unpublished/feedbacks");
       return res.data;
     },
   });
+  let unpublishedID = [];
+  feedbacks?.map((feedback) => unpublishedID.push(feedback.surveyId));
+  const feeds = unpublishedID.map((id) =>
+    surveys.find((survey) => survey._id === id)
+  );
+  console.log(unpublishedID);
+  console.log(feeds);
+
   return (
     <div>
       <SectionTitle
         title={"Survey Feedbacks"}
         subtitle={"Surveyor"}
       ></SectionTitle>
+      <div className="overflow-x-auto max-w-screen-xl mx-auto">
+        <table className="table ">
+          <thead className="bg-main">
+            <tr>
+              <th></th>
+              <th>Title</th>
+              <th>Category</th>
+              <th>Surveyor Email</th>
+              <th>Feedback</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {feeds.map((survey, i) => {
+              return (
+                <tr key={survey._id}>
+                  <th>{i + 1}</th>
+                  <td>{survey?.title}</td>
+                  <td>{survey?.category}</td>
+                  <td>{survey?.surveyorEmail}</td>
+                  <td>{feedbacks[i].feedback}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
